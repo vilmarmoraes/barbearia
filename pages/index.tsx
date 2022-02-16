ed265dbfd33e173fd771315522d3aede4e907835
vilmarmoraes <vilmarpomifrai@hotmail.com>
2022-02-16 16:09:23 -0300

    Aplicado leiaute no componente menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Menu from '../components/Menu'
+import Precos from '../components/Precos'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,7 +12,6 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Menu></Menu>
-
     </div>
   )
 }
